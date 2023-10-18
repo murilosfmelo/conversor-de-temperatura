2ee707b457c24557f57064137ac430341212e2ff
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-10-18 16:17:52 -0300

    18/10/23 atividade (outro jeito)

---
+++ codigo.py
@@ -0,0 +1,4 @@
+def c(temp):
+    return (temp * 9/5) + 32
+def f(temp):
+    return (temp - 32) * 5/9
--- converter.py
+++ converter.py
@@ -1,12 +1,13 @@
+import codigo
 while True:
     temp = float(input("Digite a temperatura: "))
 
     pergunta = int(input("Vc quer converter para celsius para Fahrenheit, ou Fahrenheit para celsius: 1/2  "))
     if pergunta == 1:
-        print("{}°C".format((temp * 9/5) + 32))
+        print("{}°C".format(codigo.c(temp)))
     else:
         if pergunta == 2:
-            print("{:.2f}°F".format((temp - 32) * 5/9))
+            print("{}°F".format(codigo.f(temp)))
         elif pergunta != 1 or pergunta != 2:
             print("Não existe essa função '-' ")
             break
